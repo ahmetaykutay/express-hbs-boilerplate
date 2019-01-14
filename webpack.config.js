883a254a105e74ad40874f53cbeed53fb1f9e4f2
ahmet aykut ay <ahmetaykutay@ahmets-MacBook-Air.local>
2019-01-14 11:08:03 +0300

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 }
